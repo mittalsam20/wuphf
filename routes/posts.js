3355596d97eebdd61bc471f6957b6a498a966c06
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-30 02:56:51 +0530

    like dislike done

--- routes/posts.js
+++ routes/posts.js
@@ -14,8 +14,6 @@ router.post("/", async(req, res) => {
     }
 })
 
-
-
 //Update a Post
 router.put("/:id", async(req, res) => {
     try {
@@ -32,10 +30,41 @@ router.put("/:id", async(req, res) => {
     }
 })
 
-
-
 //delete a Post
+router.delete("/:id", async(req, res) => {
+    try {
+        const post = Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne({ $set: req.body });
+            res.status(200).json("The post has been deleted")
+        } else {
+            res.status(403).json("You can Delete only your post")
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e)
+    }
+})
+
 //like a Post
+router.put("/:id/like", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("The post has been liked");
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("The post has been disliked");
+        }
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(err)
+    }
+
+})
+
 //get a Post
 //get timelines Post
 
