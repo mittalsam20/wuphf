133f2e8cc1b9678dce1aed35f36a7f09e10745ac
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-18 11:46:08 +0530

    right home and profile bar done css still remains

--- client/src/components/rightbar/rightbar.js
+++ client/src/components/rightbar/rightbar.js
@@ -1,5 +1,9 @@
 const Rightbar = () => {
-  return <div></div>;
+    return <div className = "rightbar" >
+        <
+        div className = "rightbarWrapper" > { user ? < ProfileRightbar / > : < HomeRightbar / > } <
+        /div> <
+        /div>
 };
 
 export default Rightbar;
