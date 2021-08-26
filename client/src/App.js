2d98a282f50ff18bbf89c6caa59defa101263de8
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-26 12:48:50 +0530

    adding url to dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import Home from "./pages/home/home";
 import { Person } from "@material-ui/icons";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 function App() {
   return (
     <>
-      <div>
-        <Home />
-        <Person />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login" exact>
+            <login />
+          </Route>
+          <Route path="/register" exact>
+            <register />
+          </Route>
+          <Route path="/profile/:username" exact>
+            <register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
