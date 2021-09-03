cb146394323257ea51a7d80f6f5102161ec04958
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-09-03 06:56:21 +0530

    restrucutre working now

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
-import "./App.css";
-
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            {user ? <Home /> : <Register />}
-          </Route>
-          <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-          <Route path="/register">
-            {user ? <Redirect to="/" /> : <Register />}
-          </Route>
-          <Route path="/profile/:username">
-            <Profile />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- server.js
+++ server.js
@@ -1,33 +1,52 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
-const cors = require("cors");
-const morgan = require("morgan")
-const app = express();
-const port = process.env.PORT || 5000;
+// const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const path = require("path");
 
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true, useUnifiedTopology: true },
-    () => console.log("Database Connected successfully..!!"))
+mongoose.connect(
+    process.env.MONGO_URL, { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-
-//Middleware
+//middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+// app.use(morgan("common"));
 
-// API Calling
-app.use("/api/user", userRoute);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploded successfully");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-
-
-
-
-app.listen(port, () => { console.log(`Server started listening at port: ${port}`) })
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});
